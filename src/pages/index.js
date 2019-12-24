1f3f1288b0fa20c1f1060c8dd24f6dc565aa571c
Justas Samuolis <samuolis.just@gmail.com>
2019-12-24 09:04:35 +0000

    Some config changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,33 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby";
 
-export default () => <div>Hello world!</div>
+export default ({
+  data: {
+    allFile: { edges },
+  }
+}) => {
+  console.log(edges);
+  return (
+    <div>Hello world!</div>
+  );
+}
+
+export const pageQuery = graphql`
+  query {
+    allFile(filter: {sourceInstanceName: {eq: "work"}}) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              date
+              path
+              title
+            }
+            id
+            excerpt(pruneLength: 250)
+          }
+        }
+      }
+    }
+  }
+`;
